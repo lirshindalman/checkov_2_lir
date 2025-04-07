a4470826233b179acc7b2a9a28664bcd35aeb3bf
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2025-04-07 16:25:48 +0300

    fix(terraform): Update FunctionAppsAccessibleOverHttps (#7084)
    * fix_CKV_AZURE_70
    
    * add test
    
    * add test
    
    * add test

--- tests/terraform/checks/resource/azure/example_FunctionAppAccessibleOverHttps_tfplan/example_fua_for_fail_ckv_azure_70.tf
+++ tests/terraform/checks/resource/azure/example_FunctionAppAccessibleOverHttps_tfplan/example_fua_for_fail_ckv_azure_70.tf
@@ -15,7 +15,7 @@ variable "location" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examples"
+  name                     = "examplestorageacc"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
