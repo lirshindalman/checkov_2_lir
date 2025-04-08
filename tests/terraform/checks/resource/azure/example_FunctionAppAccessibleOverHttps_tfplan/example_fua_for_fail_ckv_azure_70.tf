4a56479eb6a8f28159223f9455aa940a4a5cf3c7
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2025-04-08 11:07:17 +0300

    fix(terraform): rename test FunctionAppsAccessibleOverHttps  (#7085)
    * rename test
    
    * rename test

--- tests/terraform/checks/resource/azure/example_FunctionAppAccessibleOverHttps_tfplan/example_fua_for_fail_ckv_azure_70.tf
+++ tests/terraform/checks/resource/azure/example_FunctionAppAccessibleOverHttps_tfplan/example_fua_for_fail_ckv_azure_70.tf
@@ -15,7 +15,7 @@ variable "location" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
+  name                     = "examples"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
